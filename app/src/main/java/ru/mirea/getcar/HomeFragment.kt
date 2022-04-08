dea87cb9806acdad7b33874dcb80dabbd1bdd2b3
RussiaFPS <nikitakrut58@gmail.com>
2022-04-08 20:11:30 +0300

    Создана заготовка для меню и переход по фреймам

--- app/src/main/java/ru/mirea/getcar/HomeFragment.kt
+++ app/src/main/java/ru/mirea/getcar/HomeFragment.kt
@@ -1,59 +1,47 @@
 package ru.mirea.getcar
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation
+import ru.mirea.getcar.R
+import ru.mirea.getcar.onboarding.screens.FirstScreen
+import ru.mirea.getcar.onboarding.screens.SecondScreen
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val bnvMain: MeowBottomNavigation = view.findViewById(R.id.bottomNavigation)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        bnvMain.add(MeowBottomNavigation.Model(1, R.drawable.bmw_grade1))
+        bnvMain.add(MeowBottomNavigation.Model(2, R.drawable.bmw_grade1))
+        bnvMain.add(MeowBottomNavigation.Model(3, R.drawable.bmw_grade1))
+        bnvMain.add(MeowBottomNavigation.Model(4, R.drawable.bmw_grade1))
+        bnvMain.add(MeowBottomNavigation.Model(5, R.drawable.bmw_grade1))
+
+        bnvMain.show(3)
+
+        bnvMain.setOnClickMenuListener {
+            when(it.id){
+                1 -> {
+                    activity?.supportFragmentManager?.beginTransaction()
+                        ?.replace(R.id.containerFragment,FirstScreen())
+                        ?.commit()
+                }
+                2 -> {
+                    activity?.supportFragmentManager?.beginTransaction()
+                        ?.replace(R.id.containerFragment,SecondScreen())
+                        ?.commit()
                 }
             }
+        }
+
+        return view
     }
 }
