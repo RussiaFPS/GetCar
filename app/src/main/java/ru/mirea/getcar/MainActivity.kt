9176052fd0795557b25bad404e45e57d459b8ec1
RussiaFPS <nikitakrut58@gmail.com>
2022-05-06 14:37:47 +0300

    Добавлнение списка авто и настройка адаптера

--- app/src/main/java/ru/mirea/getcar/MainActivity.kt
+++ app/src/main/java/ru/mirea/getcar/MainActivity.kt
@@ -1,9 +1,7 @@
 package ru.mirea.getcar
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/ru/mirea/getcar/ShopFragment.kt
+++ app/src/main/java/ru/mirea/getcar/ShopFragment.kt
@@ -5,13 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 
 class ShopFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shop, container, false)
+        val view = inflater.inflate(R.layout.fragment_shop, container, false)
+        val listViewCars = view.findViewById<ListView>(R.id.listViewCars)
+
+        var customAdapter = CustomAdapter(this)
+        listViewCars.adapter = customAdapter
+
+        listViewCars.setOnItemClickListener { adapterView, view, i, l ->
+            Toast.makeText(context,"Item"+ customAdapter.names[i],Toast.LENGTH_LONG).show()
+        }
+
+        return view
     }
+}
+
+class CustomAdapter(private val context: ShopFragment): BaseAdapter(){
+    val names = arrayOf("Hello","id","name","123","Hello","id","name","123","Hello","id","name","123","Hello","id","name","123","Hello","id","name","name")
+
+    override fun getCount(): Int {
+        return names.size
+    }
+
+    override fun getItem(p0: Int): Any {
+        return names[p0]
+    }
+
+    override fun getItemId(p0: Int): Long {
+       return p0.toLong()
+    }
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        var inflater = context.layoutInflater
+        var view1 = inflater.inflate(R.layout.list_item,null)
+
+        var text = view1.findViewById<TextView>(R.id.item_name_cars)
+
+        text.text = names[p0]
+        return view1
+    }
+
 }
