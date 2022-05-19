2519c68f3a1c2b518d0a988d18ecfa2d01a6c9f0
n nikita <nikitakrut58@gmail.com>
2022-05-19 15:30:52 +0300

    Добавлена регистрация пользователя

--- app/src/main/java/ru/mirea/getcar/RegFragment.kt
+++ app/src/main/java/ru/mirea/getcar/RegFragment.kt
@@ -1,11 +1,16 @@
 package ru.mirea.getcar
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import org.mindrot.jbcrypt.BCrypt
 
 
 class RegFragment : Fragment() {
@@ -15,11 +20,29 @@ class RegFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_reg, container, false)
         val return_butt:ImageView = view.findViewById(R.id.return_butt)
+        val reg_conf:ImageView = view.findViewById(R.id.reg_conf)
+
+        val reg_login:EditText = view.findViewById(R.id.reg_login)
+        val reg_pass:EditText = view.findViewById(R.id.reg_pass)
+        val reg_phone:EditText = view.findViewById(R.id.reg_phone)
 
         return_butt.setOnClickListener {
             activity?.supportFragmentManager?.beginTransaction()
                 ?.replace(R.id.containerFragment, UserFragment())
                 ?.commit()
+        }
+
+        reg_conf.setOnClickListener {
+            val database = Firebase.database.reference
+            val user = Users(
+                reg_login.text.toString(), reg_pass.text.toString(),reg_phone.text.toString()
+            )
+
+            database.child("users").child(user.login.toString())
+            database.child("users").child(user.login.toString()).child("password")
+                .setValue(BCrypt.hashpw(user.pass, BCrypt.gensalt()))
+            database.child("users").child(user.login.toString()).child("phone")
+                .setValue(user.phone)
         }
 
         return view
---
+++ app/src/main/java/ru/mirea/getcar/Users.kt
@@ -0,0 +1,5 @@
+package ru.mirea.getcar
+
+data class Users(val login :String? = null, val pass : String? = null , val phone : String? = null){
+
+}
