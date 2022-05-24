cc97b218c11a87e1b209934ebb65cfaf4da3e693
n nikita <nikitakrut58@gmail.com>
2022-05-24 18:36:44 +0300

    Вывод логина на главной страничке

--- app/src/main/java/ru/mirea/getcar/MainFragment.kt
+++ app/src/main/java/ru/mirea/getcar/MainFragment.kt
@@ -1,10 +1,12 @@
 package ru.mirea.getcar
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -16,6 +18,11 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_main, container, false)
+        val user_name:TextView = view.findViewById(R.id.textView4)
+
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+        val login = sharedPref?.getString("login", "Пользователь")
+        user_name.text=login
 
         return view
     }
