dea5e438551df253e7dd486a283383fe4b9302bd
n nikita <nikitakrut58@gmail.com>
2022-05-25 12:37:24 +0300

    Выход из аккаунта

--- app/src/main/java/ru/mirea/getcar/ProfileFragment.kt
+++ app/src/main/java/ru/mirea/getcar/ProfileFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class ProfileFragment : Fragment() {
     override fun onCreateView(
@@ -16,6 +19,7 @@ class ProfileFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
         val user_name: TextView = view.findViewById(R.id.ProfileLogin)
+        val exit_button:ImageView = view.findViewById(R.id.exit_button)
 
         val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
         val login = sharedPref?.getString("login", "Пользователь")
@@ -25,6 +29,16 @@ class ProfileFragment : Fragment() {
             activity?.supportFragmentManager?.beginTransaction()
                 ?.replace(R.id.containerFragment, UserFragment())
                 ?.commit()
+        }
+
+        exit_button.setOnClickListener {
+            val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+            sharedPref?.edit()?.remove("login")?.apply()
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.containerFragment, UserFragment())
+                ?.commit()
+            Toast.makeText(context, "Вы вышли из аккаунта!", Toast.LENGTH_SHORT)
+                .show()
         }
 
         return view
