dc9effca722a4117dd2c947f2d818ccb1b920cf5
n nikita <nikitakrut58@gmail.com>
2022-05-27 19:08:27 +0300

    Вывод всех заявок в табличку

--- app/src/main/java/ru/mirea/getcar/MyReqFragment.kt
+++ app/src/main/java/ru/mirea/getcar/MyReqFragment.kt
@@ -1,16 +1,18 @@
 package ru.mirea.getcar
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TableLayout
-import android.widget.TableRow
-import android.widget.TextView
+import android.widget.*
 import androidx.core.view.marginLeft
 import androidx.core.view.marginStart
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 
 class MyReqFragment : Fragment() {
@@ -22,21 +24,86 @@ class MyReqFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_my_req, container, false)
         val table:TableLayout = view.findViewById(R.id.TableReq)
         val return_butt: ImageView = view.findViewById(R.id.return_butt)
-        val items_data: Array<String> = arrayOf<String>(
-            ( "Samsung"),
-            ("Samsung1")
-        )
+        val database = Firebase.database.reference
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+        val login = sharedPref?.getString("login", "")
 
-        for (items in items_data) {
+
+        /*database.child("orders").child("0").child(login.toString()).child("model")
+            .setValue("Test")
+        database.child("orders").child("0").child(login.toString()).child("status")
+            .setValue("Обрабатывается")*/
+
+       //val items_data: MutableList<Orders> = mutableListOf<Orders>()
+
+        database.child("orders").get().addOnSuccessListener {
+            var count = 0
+            while (count<it.childrenCount){
+                var model:String=""
+                var status:String=""
+
+                database.child("orders").child(count.toString()).child(login.toString())
+                    .child("model").get().addOnSuccessListener { it1 ->
+                        if(it1.value!=null) {
+                            model = it1.value.toString()
+                        }
+                    }
+                database.child("orders").child(count.toString()).child(login.toString())
+                    .child("status").get().addOnSuccessListener { it2 ->
+                        if(it2.value!=null){
+                            status = it2.value.toString()
+
+                            val tr = TableRow(context)
+                            val md = TextView(context)
+                            md.text = model
+                            tr.addView(md)
+                            val st = TextView(context)
+                            st.text = status
+                            tr.addView(st)
+                            table.addView(tr)
+                        }
+                    }
+
+                count++
+            }
+        }
+
+
+        /*database.child("orders").get().addOnSuccessListener {
+            var count = 0
+            while (count<it.childrenCount){
+                var model:String=""
+                var status:String=""
+
+                database.child("orders").child(count.toString()).child(login.toString())
+                    .child("model").get().addOnSuccessListener { it1 ->
+                        if(it1.value.toString()!="") {
+                            model = it1.value.toString()
+                        }
+                }
+                database.child("orders").child(count.toString()).child(login.toString())
+                    .child("status").get().addOnSuccessListener { it2 ->
+                        if(it2.value!=null){
+                            status = it2.value.toString()
+                        }
+                    }
+
+                Toast.makeText(context, status, Toast.LENGTH_SHORT)
+                    .show()
+                count++
+            }
+        }*/
+
+        /*for (items in items_data) {
             val tr = TableRow(context)
             val model = TextView(context)
-            model.text = items
+            model.text = items.model
             tr.addView(model)
             val status = TextView(context)
-            status.text = "hello"
+            status.text = items.status
             tr.addView(status)
             table.addView(tr)
-        }
+        }*/
 
         return_butt.setOnClickListener {
             activity?.supportFragmentManager?.beginTransaction()
---
+++ app/src/main/java/ru/mirea/getcar/Orders.kt
@@ -0,0 +1,5 @@
+package ru.mirea.getcar
+
+data class Orders(val model :String? = null, val status : String? = null) {
+
+}
