4a8d2ccde9bb717d6e509e4946a5e27daa4af72c
n nikita <nikitakrut58@gmail.com>
2022-05-31 18:02:32 +0300

    Отправка заявки

--- app/src/main/java/ru/mirea/getcar/ChoiceCarFragment.kt
+++ app/src/main/java/ru/mirea/getcar/ChoiceCarFragment.kt
@@ -1,5 +1,6 @@
 package ru.mirea.getcar
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -23,8 +24,11 @@ class ChoiceCarFragment : Fragment() {
         val ChoiceCarImage:ImageView = view.findViewById(R.id.ChoiceCarImage)
         val ChoiceCarCost:TextView = view.findViewById(R.id.ChoiceCarCost)
         val ChoiceCarDesc:TextView = view.findViewById(R.id.ChoiceCarDesc)
+        val choice_butt_req:ImageView = view.findViewById(R.id.choice_butt_req)
         val bundle = arguments
         val idCar = bundle!!.getInt("idCar")
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+        val login = sharedPref?.getString("login", "Пользователь")
         val database = Firebase.database.reference
 
         database.child("cars").child(idCar.toString()).child("model").get().addOnSuccessListener {
@@ -40,6 +44,23 @@ class ChoiceCarFragment : Fragment() {
             }
         }
 
+       choice_butt_req.setOnClickListener {
+           if (login!="Пользователь"){
+               database.child("orders").get().addOnSuccessListener {
+                   val count:Long = it.childrenCount
+
+                   database.child("orders").child(count.toString()).child(login.toString()).child("model")
+                        .setValue(ChoiceCarName.text.toString())
+                    database.child("orders").child(count.toString()).child(login.toString()).child("status")
+                        .setValue("Обрабатывается")
+                   Toast.makeText(context, "Заявка создана!", Toast.LENGTH_SHORT)
+                       .show()
+               }
+           }else{
+               Toast.makeText(context, "Для этой функции нужно авторизоваться!", Toast.LENGTH_SHORT)
+                   .show()
+           }
+       }
 
         return_butt.setOnClickListener {
             activity?.supportFragmentManager?.beginTransaction()
