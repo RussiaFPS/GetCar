86b90e2a1850d9a7d87101694cc2590172e80ccc
n nikita <nikitakrut58@gmail.com>
2022-06-02 12:32:40 +0300

    Добавление карты

--- app/src/main/java/ru/mirea/getcar/LocationFragment.kt
+++ app/src/main/java/ru/mirea/getcar/LocationFragment.kt
@@ -1,10 +1,14 @@
 package ru.mirea.getcar
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
+
+var mapView: MapView? = null
 
 class LocationFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +16,31 @@ class LocationFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false)
+        val view = inflater.inflate(R.layout.fragment_location, container, false)
+
+        mapView = view.findViewById(R.id.mapView)
+        mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
+
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mapView?.onDestroy()
     }
 }
